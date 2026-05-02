e92f25f43e007ad8c6e72ae540c7b3a9fd751caa
erwinklein94 <erwinklein1994@gmail.com>
2026-05-02 20:45:45 -0300

    correção de bugs grandes

---
+++ js/script.js
@@ -0,0 +1,5 @@
+/* Compatibilidade: o código-fonte foi reorganizado.
+   Produção: js/dist/app.bundle.js
+   Desenvolvimento: js/main.js lista a ordem dos arquivos-fonte.
+*/
+console.info('UniSegPub: use js/dist/app.bundle.js em produção ou js/main.js para consultar a ordem dos fontes.');
